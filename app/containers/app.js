457b3d31bd89a81e23cf1459ca8ac5a37ed0a3d2
Dmytro Novikov <nodm@ciklum.com>
2017-01-07 15:45:30 +0200

    Navigation routes were added to the common Redux store

--- app/containers/app.js
+++ app/containers/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { connect, Provider } from 'react-redux';
 
 import reducers from '../reducers/';
 
@@ -12,29 +12,28 @@ import Minesweeper from './minesweeper';
 import Puzzle from './puzzle';
 import MainMenu from './mainMenu';
 
+const RouterWithRedux = connect()(Router);
 const store = createStore(reducers);
 
 export default class App extends Component {
   render() {
     return(
       <Provider store={store}>
-        <Router>
+        <RouterWithRedux>
           <Scene key="tabBar" tabs={true} tabBarStyle={style.tabBarStyle}>
             <Scene
               key="tabMinesweeper"
-              hideNavBar={true}
               title="Minesweeper"
               icon={params => TabBarItem({ ...params, iconName: 'flag' })}
             >
-              <Scene key="pageMinesweeper" component={Minesweeper} title="Minesweeper" />
+              <Scene key="pageMinesweeper" component={Minesweeper} title="Minesweeper" hideNavBar />
             </Scene>
             <Scene
               key="tabPuzzle"
-              hideNavBar={true}
               title="Puzzle"
               icon={params => TabBarItem({ ...params, iconName: 'extension' })}
             >
-              <Scene key="pagePuzzle" component={Puzzle} title="Puzzle" />
+              <Scene key="pagePuzzle" component={Puzzle} title="Puzzle" hideNavBar />
             </Scene>
             <Scene
               key="tabSettings"
@@ -44,7 +43,7 @@ export default class App extends Component {
               <Scene key="pageFour" component={MainMenu} title="Settings" />
             </Scene>
           </Scene>
-        </Router>
+        </RouterWithRedux>
       </Provider>
     );
   }
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
+import routes from './routes';
 import minesweeper from './minesweeper';
 import puzzle from './puzzle';
 
 export default combineReducers({
+  routes,
   minesweeper,
   puzzle,
 });
